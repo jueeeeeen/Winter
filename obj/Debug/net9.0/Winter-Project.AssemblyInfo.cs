b5309cc5755cad32bd4f09d6fa9cd79ee783afec
MustMark <Superpeerawat.mm@gmail.com>
2025-02-13 00:43:35 +0700

    merge version

--- obj/Debug/net9.0/Winter-Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Winter-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Winter-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe1514f569310f6d10e661bead9a9753649f2c8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f5c05a9a2c03b402ebd78df0f8982bcc4af643")]
 [assembly: System.Reflection.AssemblyProductAttribute("Winter-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Winter-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

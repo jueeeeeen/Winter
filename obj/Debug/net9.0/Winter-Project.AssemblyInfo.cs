2170fcb7acd628e216e86ea2253c9b22d1aacf89
laluern <earn.sataporn@gmail.com>
2025-02-14 12:28:30 +0700

    edit page (ui)

--- obj/Debug/net9.0/Winter-Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Winter-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Winter-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30ca4c87a9854f3bb8676862c0a57a6f7d2a4b82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+698a154329f6e29e7bfaefc5040c9926357ed838")]
 [assembly: System.Reflection.AssemblyProductAttribute("Winter-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Winter-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

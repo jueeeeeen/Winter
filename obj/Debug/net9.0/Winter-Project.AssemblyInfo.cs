424ad8c7e67df098d5907f6719d37371d282dfe3
MustMark <Superpeerawat.mm@gmail.com>
2025-02-17 02:09:54 +0700

    add create page

--- obj/Debug/net9.0/Winter-Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Winter-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Winter-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6747d75ceb9e978d16c3dfb3b625c8630d844b43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d15d815282392031a449b6a0854176ba581954")]
 [assembly: System.Reflection.AssemblyProductAttribute("Winter-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Winter-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

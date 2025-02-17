2e7bf6f8649af4a829885e6d98100d0f5ef7c6b8
Thirdsupakarn <supakarn.hatyaiwit@gmail.com>
2025-02-17 10:27:07 +0700

    clean up

--- obj/Debug/net9.0/Winter-Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Winter-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Winter-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187dea521221f4c9f8ddb3e742ae4e5ac1065961")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a962b384a06821e742d246d342db7d45e3f4228f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Winter-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Winter-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

326476be91f54d1d3b23221173a9dc6bdcbaa4ae
jueeeeeen <jinchowchow2547@gmail.com>
2025-12-17 14:59:15 +0700

    update README

--- Models/ActivityModel.cs
+++ Models/ActivityModel.cs
@@ -4,13 +4,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Winter_Project.Models
 {
     public class ActivityModel
-    {
-        // private static int _next_id = 1;
-        
+    {        
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Activity_id {get; set;}
-
         public string Owner {get; set;} = string.Empty;
         public string Title {get; set;} = string.Empty;
         public string Detail {get; set;} = string.Empty;
@@ -22,13 +19,8 @@ namespace Winter_Project.Models
         public int Max_member {get; set;}
         public bool Approval {get; set;}
         public List<string> Tags {get; set;} = new List<string>();
-
         public RequirementModel Requirement { get; set; } = new RequirementModel();
-        
         public ICollection<ParticipantModel> Participants { get; set; } = new List<ParticipantModel>();
-
         public string Status { get; set; } = string.Empty;
     }
-
-
 }
